58aa62a53386e90a15c5ab5ca67072fc04b4e4c4
salak402 <salak402@salak402-38.salasfr.itm.edu.co>
2024-11-14 15:10:25 -0500

    Hice un par de cambios en los void: Taxi, Vans y Buseta. Además, agregué dos nuevos: Rutas e Impresion; el primero, es un simple menú que podríamos usar para que el propio usuario elija la ruta que quiera ser pasajero, mientras que, la segunda, imprime todos los resultados de los vehículos, para que sea mucho más ordenado y no estén en cada uno de los vehículos. CONSULTA: no tengo muy claro cómo añadir tus cambios con los mios, es decir, al momento de ingresar la ruta específica que se apliquen los "void cambios" que hiciste, si puedes organizar esa parte sería bueno.

--- src/proyectofinal/Vehiculo.java
+++ src/proyectofinal/Vehiculo.java
@@ -30,7 +30,7 @@ public class Vehiculo {
         distanciaSubidaSantaElenaCambioUno = 20;
         distanciaSubidaSantaElenaCambioDos = 20;
         distanciaSubidaSantaElenaCambioTres = 10;
-        
+
         //el total de subida Palmas es de: 55
         distanciaSubidaPalmasCambioUno = 15;
         distanciaSubidaPalmasCambioDos = 15;
@@ -46,45 +46,33 @@ public class Vehiculo {
 
     } // END VOID GASOLINA
 
-    int cuposTaxi;
+    int pasajerosTaxi = 4;
     double consumoAutopistaTaxi;
     double consumoSantaELenaYPalmasTaxi;
 
     public void Taxi() {
 
-        cuposTaxi = 4;
-
-        System.out.println("Pasajeros del Taxi: " + cuposTaxi);
-
-        consumoAutopistaTaxi = cuposTaxi * 1.20;
-        consumoSantaELenaYPalmasTaxi = cuposTaxi * 1.28;
-
-        System.out.println("Consumo en la ruta Autopista del Taxi: " + consumoAutopistaTaxi + " Galones");
-        System.out.println("Consumo en la ruta Santa Elena y Palmas del Taxi: " + consumoSantaELenaYPalmasTaxi + " Galones");
+        consumoAutopistaTaxi = pasajerosTaxi * 1.20;
+        consumoSantaELenaYPalmasTaxi = pasajerosTaxi * 1.28;
 
     } // END VOID TAXI
 
+    Random randomVans = new Random();
+
+    int pasajerosVans = randomVans.nextInt(10) + 1;
     double consumoAutopistaVans;
     double consumoSantaELenaYPalmasVans;
 
     public void Vans() {
 
-        System.out.println("");
-
-        Random randomNumbers = new Random();
-
-        int pasajero = randomNumbers.nextInt(10) + 1;
-
-        System.out.println("Pasajeros de la Vans: " + pasajero);
-
-        consumoAutopistaVans = pasajero * 1.08;
-        consumoSantaELenaYPalmasVans = pasajero * 1.11;
-
-        System.out.println("Consumo en la ruta Autopista en la Vans: " + consumoAutopistaVans + " Galones");
-        System.out.println("Consumo en la ruta Santa Elena y Palmas en la Vans: " + consumoSantaELenaYPalmasVans + " Galones");
+        consumoAutopistaVans = pasajerosVans * 1.08;
+        consumoSantaELenaYPalmasVans = pasajerosVans * 1.11;
 
     } // END VOID VANS
 
+    Random randomBuseta = new Random();
+
+    int pasajerosBuseta = randomBuseta.nextInt(15) + 1;
     double consumoAutopistaBuseta;
     double consumoSantaELenaYPalmasBuseta;
 
@@ -92,17 +80,8 @@ public class Vehiculo {
 
         System.out.println("");
 
-        Random randomNumbers = new Random();
-
-        int pasajero = randomNumbers.nextInt(15) + 1;
-
-        System.out.println("Pasajeros de la Buseta: " + pasajero);
-
-        consumoAutopistaBuseta = pasajero * 1.12;
-        consumoSantaELenaYPalmasBuseta = pasajero * 1.15;
-
-        System.out.println("Consumo en la ruta Autopista en la Buseta: " + consumoAutopistaBuseta + " Galones");
-        System.out.println("Consumo en la ruta Santa Elena y Palmas en la Buseta: " + consumoSantaELenaYPalmasBuseta + " Galones");
+        consumoAutopistaBuseta = pasajerosBuseta * 1.12;
+        consumoSantaELenaYPalmasBuseta = pasajerosBuseta * 1.15;
 
     } // END VOID BUSETA
 
@@ -113,7 +92,7 @@ public class Vehiculo {
     double consumoCambioAutopista;
     double consumoCambioSantaElena;
     double consumoCambioPalmas;
-            
+
     public void Cambios() {
         distanciaRutas();
 
@@ -123,20 +102,81 @@ public class Vehiculo {
         cambioDos = .02;
         cambioTres = .01;
 
-        consumoCambioAutopista = (distanciaSubidaAutopistaCambioUno * cambioUno)+(distanciaSubidaAutopistaCambioDos * cambioDos)+(distanciaSubidaAutopistaCambioTres * cambioTres);
-        consumoCambioSantaElena = (distanciaSubidaSantaElenaCambioUno * cambioUno)+(distanciaSubidaSantaElenaCambioDos * cambioDos)+(distanciaSubidaSantaElenaCambioTres * cambioTres);
-        consumoCambioPalmas = (distanciaSubidaPalmasCambioUno * cambioUno)+(distanciaSubidaPalmasCambioDos * cambioDos)+(distanciaSubidaPalmasCambioTres * cambioTres);
-        
+        consumoCambioAutopista = (distanciaSubidaAutopistaCambioUno * cambioUno) + (distanciaSubidaAutopistaCambioDos * cambioDos) + (distanciaSubidaAutopistaCambioTres * cambioTres);
+        consumoCambioSantaElena = (distanciaSubidaSantaElenaCambioUno * cambioUno) + (distanciaSubidaSantaElenaCambioDos * cambioDos) + (distanciaSubidaSantaElenaCambioTres * cambioTres);
+        consumoCambioPalmas = (distanciaSubidaPalmasCambioUno * cambioUno) + (distanciaSubidaPalmasCambioDos * cambioDos) + (distanciaSubidaPalmasCambioTres * cambioTres);
+
         System.out.println("En la subida via Autopista el consumo adicional fue de: " + consumoCambioAutopista + " galones");
         System.out.println("En la subida via Santa Elena el consumo adicional fue de: " + consumoCambioSantaElena + " galones");
         System.out.println("En la subida via Palmas el consumo adicional fue de: " + consumoCambioSantaElena + " galones");
     }//END VOID CAMBIOS
 
+    int distanciaAutopista = 45;
+    int distanciaSantaElena = 50;
+    int distanciaPalmas = 55;
+    int ruta;
+
+    public void Rutas() {
+
+        System.out.println("POR FAVOR ELIJA LA RUTA QUE QUIERA TOMAR:");
+        System.out.println("1. Autopista \n"
+                + //
+                "2. Santa Elena \n"
+                + //
+                "3. Las Palmas ");
+        ruta = Datos.nextInt();
+
+        if (ruta == 1) {
+            System.out.println("");
+            System.out.println("Usted eligio la ruta: Autopista");
+        }
+        if (ruta == 2) {
+            System.out.println("");
+            System.out.println("Usted eligio la ruta: Santa Elena");
+        }
+        if (ruta == 3) {
+            System.out.println("");
+            System.out.println("Usted eligio la ruta: Las Palmas");
+        }
+        if (ruta != 1 && ruta != 2 && ruta != 3) {
+            System.out.println("");
+            System.out.println("POR FAVOR ELIJA ALGUNA DE LAS TRES RUTA");
+        }
+    } // END VOID RUTAS
+
+    public void Impresion() {
+
+        System.out.println("=============RESULTADOS=============");
+
+        // IMPRESION DE LA TAXI
+        System.out.println("Pasajeros del Taxi: " + pasajerosTaxi);
+        System.out.println("Consumo en la ruta Autopista del Taxi: " + consumoAutopistaTaxi + " Galones");
+        System.out.println(
+                "Consumo en la ruta Santa Elena y Palmas del Taxi: " + consumoSantaELenaYPalmasTaxi + " Galones");
+
+        // IMPRESION DE LA VANS
+        System.out.println("");
+        System.out.println("Pasajeros de la Vans: " + pasajerosVans);
+        System.out.println("Consumo en la ruta Autopista en la Vans: " + consumoAutopistaVans + " Galones");
+        System.out.println(
+                "Consumo en la ruta Santa Elena y Palmas en la Vans: " + consumoSantaELenaYPalmasVans + " Galones");
+
+        // IMPRESION DE LA BUSETA
+        System.out.println("");
+        System.out.println("Pasajeros de la Buseta: " + pasajerosBuseta);
+        System.out.println("Consumo en la ruta Autopista en la Buseta: " + consumoAutopistaBuseta + " Galones");
+        System.out.println(
+                "Consumo en la ruta Santa Elena y Palmas en la Buseta: " + consumoSantaELenaYPalmasBuseta + " Galones");
+    } // END VOID IMPRESION
+
     public static void main(String[] args) {
         Vehiculo Prueba = new Vehiculo();
+        Prueba.Rutas();
         Prueba.Taxi();
         Prueba.Vans();
         Prueba.Buseta();
         Prueba.Cambios();
+        System.out.println("");
+        Prueba.Impresion();
     }
 }
